d7bcf572573d836060b8f6a973a43ca0931c954f
Ram Be'er <ramron123@gmail.com>
2026-04-27 14:55:21 +0300

    feat: add JSON run report

---
+++ src/reporters/jsonReporter.js
@@ -0,0 +1,10 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+async function writeJsonReport(report, outputPath) {
+  await fs.mkdir(path.dirname(outputPath), { recursive: true });
+  await fs.writeFile(outputPath, `${JSON.stringify(report, null, 2)}\n`, "utf8");
+  return outputPath;
+}
+
+module.exports = { writeJsonReport };
